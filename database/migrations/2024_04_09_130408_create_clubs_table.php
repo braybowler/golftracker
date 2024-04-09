90e9424e774c094f8a222f96ebccf34f5b6cac41
braybowler <braybowler1995@gmail.com>
2024-04-09 18:16:09 -0400

    migration for clubs table

---
+++ database/migrations/2024_04_09_130408_create_clubs_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\Models\Club;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('clubs', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('bag_id')->constrained()->cascadeOnDelete();
+            $table->enum('type', Club::TYPE_OPTIONS)->nullable();
+            $table->string('make')->nullable();
+            $table->string('model')->nullable();
+            $table->tinyInteger('average_carry')->nullable();
+            $table->tinyInteger('average_total')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('clubs');
+    }
+};
